4238f0c2279f3cf4438c3394e45bb1903e27d409
Christopher Davis <Christopher.Davis.013@gmail.com>
2025-01-30 11:39:17 -0500

    Added protobuf to noExternal

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   external: ["crc", "ste-simple-events", "tslog"],
-  noExternal: ["@buf/meshtastic_protobufs.bufbuild_es"],
+  noExternal: [
+    "@buf/meshtastic_protobufs.bufbuild_es",
+    "@jsr/meshtastic__protobufs",
+    "@meshtastic/protobufs",
+    "@bufbuild/protobuf",
+  ],
   format: "esm",
   sourcemap: true,
   clean: true,
